d7ed86808d8f677669b7198026916bc66814931d
maaz <mazzasimqw@gmail.com>
2024-07-28 15:35:08 +0500

    updated routes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,9 @@ import colors from 'tailwindcss/colors';
 
 const config: Config = {
   content: [
-
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './Components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
 
     // Path to Tremor module
     './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
@@ -19,31 +18,31 @@ const config: Config = {
         // light mode
         tremor: {
           brand: {
-            faint: '#0B1229',
-            muted: colors.blue[950],
-            subtle: colors.blue[800],
+            faint: colors.blue[50],
+            muted: colors.blue[200],
+            subtle: colors.blue[400],
             DEFAULT: colors.blue[500],
-            emphasis: colors.blue[400],
-            inverted: colors.blue[950],
+            emphasis: colors.blue[700],
+            inverted: colors.white,
           },
           background: {
-            muted: '#131A2B',
-            subtle: colors.gray[800],
-            DEFAULT: colors.gray[900],
-            emphasis: colors.gray[300],
+            muted: colors.gray[50],
+            subtle: colors.gray[100],
+            DEFAULT: colors.white,
+            emphasis: colors.gray[700],
           },
           border: {
-            DEFAULT: colors.gray[800],
+            DEFAULT: colors.gray[200],
           },
           ring: {
-            DEFAULT: colors.gray[800],
+            DEFAULT: colors.gray[200],
           },
           content: {
-            subtle: colors.gray[600],
+            subtle: colors.gray[400],
             DEFAULT: colors.gray[500],
-            emphasis: colors.gray[200],
-            strong: colors.gray[50],
-            inverted: colors.gray[950],
+            emphasis: colors.gray[700],
+            strong: colors.gray[900],
+            inverted: colors.white,
           },
         },
         // dark mode
